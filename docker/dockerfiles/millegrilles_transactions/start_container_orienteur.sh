3fa1dc6e8d73d805d4f2662c9af62cf07fc98e5d
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-09-16 14:19:04 -0400

    Ajout service orienteur

--- docker/dockerfiles/millegrilles_transactions/start_container_orienteur.sh
+++ docker/dockerfiles/millegrilles_transactions/start_container_orienteur.sh
@@ -6,5 +6,5 @@ ARCH=`uname -m`
 
 # On va executer l'orienteur de transaction plutot que le consignateur de transaction
 
-docker run $REPO/$NAME.$ARCH:$VERSION millegrilles/processus/OrienteurTransaction.py
+docker run $REPO/$NAME.$ARCH:$VERSION python ./millegrilles/processus/OrienteurTransaction.py
 
