10ae0cb3e1fd3ac9bc34d449c7acc0fee2ec7202
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-13 16:34:40 -0500

    Update script python build, rabbitmq

--- docker/dockerfiles/millegrilles_transactions/build.sh
+++ docker/dockerfiles/millegrilles_transactions/build.sh
@@ -9,12 +9,19 @@ IMAGENAME="$NAME.$ARCH:$VERSION"
 # Copier les fichiers source dans un repertoire temporaire pour
 # les inclure a l'image.
 GIT_NAME=MilleGrilles.consignation.python
-GIT_FOLDER=~/git/$GIT_NAME
-git -C $GIT_FOLDER pull
+GIT_PATH=ssh://repository.maple.mdugre.info/var/lib/git/$GIT_NAME.git
+SRC_FOLDER=tmp_src
+GIT_FOLDER=$SRC_FOLDER/$GIT_NAME
 
-mkdir src
-cp $GIT_FOLDER/requirements.txt src/
-cp -r $GIT_FOLDER/millegrilles/ src/
+mkdir $SRC_FOLDER
+git -C $SRC_FOLDER clone $GIT_PATH
+if [ $? -ne "0" ]; then
+  echo "Erreur extraction code via git"
+  exit -1
+fi
+
+rm -rf $GIT_FOLDER/.git
+ln -s $GIT_FOLDER src
 
 docker build -t $IMAGENAME .
 
@@ -30,4 +37,5 @@ else
 fi
 
 # Cleanup, effacer les repertoires de travail
-rm -rf src
+rm -rf $SRC_FOLDER src
+
