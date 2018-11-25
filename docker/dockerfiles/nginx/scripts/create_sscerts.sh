2f772dad3b5abec9d75dabd420937dd7ae7d2f6e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-25 10:25:42 -0500

    Ajout script pour creer dummy certs

---
+++ docker/dockerfiles/nginx/scripts/create_sscerts.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd ../dummy_certs
+
+openssl genrsa -des3 -out server.key 2048
+openssl req -new -key server.key -out server.csr
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
+cp server.key ssl.maple.millegrilles.key
+cp server.crt ssl.maple.millegrilles.crt
+
+# Cleanup
+rm server.*
