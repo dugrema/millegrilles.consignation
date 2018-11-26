5910e9b244197d2a9a1f201a7c98235659fbd365
Mathieu Dugre <mathieu@infraserv1.maple.mdugre.info>
2018-11-26 08:33:52 -0500

    Ajout compteur pour volumes crees avec succes

--- docker/scripts/setup-volumes.sh
+++ docker/scripts/setup-volumes.sh
@@ -4,8 +4,16 @@ VOLUMES=(`cat volumes.txt`)
 
 echo "Preparation des volumes [${VOLUMES[*]}] dans docker"
 
+ERREURS=0
+REUSSIS=0
+
 for vol in ${VOLUMES[@]}; do
   docker volume create $vol
+  if [ $? -eq "0" ]; then
+    ((REUSSIS++))  # Compter le nombre d'echecs
+  else
+    ((ERREURS++))
+  fi
 done
 
-echo "Setup volumes complete"
+echo "Setup $REUSSIS volumes complete, $ERREURS erreurs"
