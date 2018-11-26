2bf5f7bf3e42335ddf3baa0dee683b3d9d171c1e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-26 15:25:27 -0500

    Configuration image RabbitMQ pour supporter port 443

--- docker/dockerfiles/rabbitmq/scripts/setup.sh
+++ docker/dockerfiles/rabbitmq/scripts/setup.sh
@@ -10,6 +10,15 @@ mkdir -p $BUNDLE_FOLDER
 cp $SRC_FOLDER/config/rabbitmq.config /etc/rabbitmq
 cp $SRC_FOLDER/config/definitions.json $BUNDLE_FOLDER/
 
+# Installer setcap pour permettre de demarrer sur port 443
+export http_proxy=http://192.168.1.28:8000
+apt-get update
+apt-get install -y libcap2-bin
+setcap 'cap_net_bind_service=+ep' /usr/lib/rabbitmq/lib/rabbitmq_server-3.7.8/sbin/rabbitmq-server
+setcap 'cap_net_bind_service=+ep' /usr/lib/erlang/erts-9.3.3.3/bin/epmd
+setcap 'cap_net_bind_service=+ep' /usr/lib/erlang/erts-9.3.3.3/bin/beam.smp
+apt-get autoclean
+
 # Cleanup, supprimer le repertoire src/
 cd /
 rm -rf $SRC_FOLDER
