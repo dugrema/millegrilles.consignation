22274dea7f15a93e7053bb3830107557640a2539
Mathieu Dugre <mathieu@infraserv1.maple.mdugre.info>
2018-11-27 15:29:13 -0500

    Scripts pour trigger horaire (cron)

---
+++ scripts/trigger_horaire.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run --rm -it --env-file /home/millegrilles/env.txt \
+registry.maple.mdugre.info:5000/millegrilles_domaines_python.x86_64:v0.5.1 \
+demarrer_declencheur.py -m senseurspassifs_maj_horaire
