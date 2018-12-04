0ba7f9f06804dcb7aa868577a419d55d27f09a2c
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-12-04 15:32:24 -0500

    Suppression dockerfiles domaines et python.
    Les Dockerfiles sont rendues directement dans les repertoires avec le 
    code source python.

--- docker/dockerfiles/millegrilles_python/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Importer le nom du repository, image et version
-source image_info.txt
-ARCH=`uname -m`
-
-#docker run --entrypoint python3 $REPO/$NAME.$ARCH:$VERSION millegrilles/transaction/ConsignateurTransaction.py
-docker run $REPO/$NAME.$ARCH:$VERSION
