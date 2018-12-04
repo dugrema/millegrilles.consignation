4f0efe085916def370f77e36fecab99e9a2d5ddd
Mathieu Dugre <mathieu@infraserv1.maple.mdugre.info>
2018-12-04 15:28:27 -0500

    Fix scripts, copies mauvaise place

--- scripts/trigger_quotidien.sh
+++ scripts/trigger_quotidien.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-docker run --rm -it --env-file /home/millegrilles/env.txt \
+LOG=/home/millegrilles/logs/trigger_quotidien
+
+echo -e "\n" >> $LOG.log
+echo `date` >> $LOG.log
+echo -e "\n" >> $LOG.err
+echo `date` >> $LOG.err
+
+docker run --rm --env-file /home/millegrilles/env.txt \
 registry.maple.mdugre.info:5000/millegrilles_domaines_python.x86_64:v0.5.1 \
-demarrer_declencheur.py -m senseurspassifs_maj_quotidienne
+demarrer_declencheur.py -m senseurspassifs_maj_quotidienne \
+>> $LOG.log 2>> $LOG.err
+
