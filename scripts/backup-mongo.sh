577340b7e9f2fddb2650bc51b5699572e9142086
Mathieu Dugre <mathieu@infraserv1.maple.mdugre.info>
2018-12-17 11:39:42 -0500

    Update script backup, mot de passe externe

--- scripts/backup-mongo.sh
+++ scripts/backup-mongo.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z $MONGO_PASSWORD ]; then
+  echo Il faut fournir le mot de passe mongo dans MONGO_PASSWORD
+  exit 1
+fi
+
 BACKUP_MAPPED_FOLDER=/var/lib/backup_docker
 DATE=`date +"%Y%m%d-%H%M"`
 BACKUP_ROOT=/mnt/backup
@@ -14,6 +19,13 @@ chmod 770 $BACKUP_MAPPED_FOLDER/$BACKUP_LEAF
 BACKUP_FOLDER=$BACKUP_ROOT/$BACKUP_LEAF
 
 IMAGE=mongo
-COMMAND="mongodump --uri=mongodb://backup:pydNOD090quo@192.168.1.28 --ssl --sslAllowInvalidCertificates -o $BACKUP_FOLDER --oplog --gzip"
+COMMAND="mongodump --uri=mongodb://backup:$MONGO_PASSWORD@192.168.1.28 --ssl --sslAllowInvalidCertificates -o $BACKUP_FOLDER --oplog --gzip"
 
 docker run -t --rm -v $BACKUP_MAPPED_FOLDER:$BACKUP_ROOT $IMAGE $COMMAND
+
+if [ $? -eq 0 ]; then
+  echo "Backup complete avec succes"
+else
+  exit 2
+fi
+
