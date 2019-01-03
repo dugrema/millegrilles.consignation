f516fa6c584b3a03fe212b7d1973f59b13fe2675
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-03 14:52:53 -0500

    Utilisation docker-compose.
    Creation des fichiers d'environnements et script pour l'utiliser.

---
+++ docker/scripts/preparer_env_file.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+DOCKER_COMPOSE=/usr/local/bin/docker-compose
+
+if [ -z $1 ]; then
+  echo "Il faut fournir le nom de l'environnement"
+  exit 1
+fi
+
+ENV_NAME=$1
+if [ ! -f $ENV_NAME.env ]; then
+  echo "Le fichier $ENV_NAME.env n'existe pas"
+  exit 2
+fi
+
+
+# Exporter les variables du fichier d'environnement
+source $ENV_NAME.env
+`$(cut -d= -f1 $ENV_NAME.env)`
+
+$DOCKER_COMPOSE config > $ENV_NAME.yml
+
+echo "Fichier $ENV_NAME.yml cree."
+exit 0
