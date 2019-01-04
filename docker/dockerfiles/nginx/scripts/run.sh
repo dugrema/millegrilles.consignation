2de8fb4e7d6c867a360bd1ab42c5f5be30c22aa2
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-04 10:40:28 -0500

    Travail sur nouvelle approche config env.

--- docker/dockerfiles/nginx/scripts/run.sh
+++ docker/dockerfiles/nginx/scripts/run.sh
@@ -2,11 +2,23 @@
 
 SECRET=/run/secrets
 NGINX=/usr/sbin/nginx
+CONF=/etc/nginx/conf.d
 
 if [[ ! -d $SECRET ]]; then
   echo "Folder secret n'existe pas, on installe les certificats de test"
   mkdir -p $SECRET
   cp $APP_BUNDLE_DIR/dummy_certs/* $SECRET
+fi
+
+# Effectuer substitution des variables d'Environnement
+if [ -z $NGINX_NOOVERRIDE_CONF ]; then
+  if [ -z $NGINX_CONFIG_FILE ]; then
+    echo "Utilisation fichier configuration bundle default.conf"
+    envsubst < $APP_BUNDLE_DIR/sites-available/default.conf > $CONF/default.conf
+  else
+    echo "Utilisation fichier configuration dans bundle: $NGINX_CONFIG_FILE"
+    envsubst < $APP_BUNDLE_DIR/sites-available/$NGINX_CONFIG_FILE > $CONF/default.conf
+  fi
 fi
 
 echo "Demarrage de nginx"
--- docker/dockerfiles/nginx/scripts/setup.sh
+++ docker/dockerfiles/nginx/scripts/setup.sh
@@ -8,7 +8,7 @@ mkdir -p $APP_BUNDLE_DIR
 mkdir -p $APP_SOURCE_DIR
 
 rm /etc/nginx/conf.d/default.conf
-mv $APP_SOURCE_DIR/sites-available/* /etc/nginx/conf.d
+mv $APP_SOURCE_DIR/sites-available $APP_BUNDLE_DIR
 
 echo "Copier run.sh, dummy_certs vers $APP_BUNDLE_DIR"
 mv $APP_SOURCE_DIR/scripts/run.sh $APP_BUNDLE_DIR
@@ -16,7 +16,7 @@ chmod u+x $APP_BUNDLE_DIR/run.sh
 
 mv $APP_SOURCE_DIR/dummy_certs $APP_BUNDLE_DIR
 echo "Fichiers dans $APP_BUNDLE_DIR :"
-ls -la $APP_BUNDLE_DIR
+find $APP_BUNDLE_DIR
 
 # Cleanup
 rm -rf $APP_SOURCE_DIR
