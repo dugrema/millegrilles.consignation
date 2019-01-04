6e701d89a4906faa6eac7065f58b56932cccb540
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-04 14:35:39 -0500

    Restore fonctionalite create_signing.
    Les commandes etaient en commentaire.

--- certificates/signing/create_signing.sh
+++ certificates/signing/create_signing.sh
@@ -5,21 +5,21 @@ PRIVATE_PATH=~/certificates/millegrilles/privkeys
 CAKEY=$PRIVATE_PATH/millegrilles_signing_key.pem
 let "DAYS=365 * 5"  # 5 ans
 
-#if [ -f $CAKEY ]; then
-#  echo "Cle existe deja, on abort."
-#  exit 1
-#fi
+if [ -f $CAKEY ]; then
+  echo "Cle existe deja, on abort."
+  exit 1
+fi
 
-#openssl req \
-#        -config $CNF_FILE \
-#        -newkey rsa:8192 \
-#        -sha512 \
-#        -out millegrilles_signing_req.csr -outform PEM \
-#        -keyout $CAKEY -keyform PEM
+openssl req \
+        -config $CNF_FILE \
+        -newkey rsa:8192 \
+        -sha512 \
+        -out millegrilles_signing_req.csr -outform PEM \
+        -keyout $CAKEY -keyform PEM
 
 # Creer backup de la cle
-#chmod 400 $CAKEY
-#cp $CAKEY $CAKEY.`date +%Y%m%d`
+chmod 400 $CAKEY
+cp $CAKEY $CAKEY.`date +%Y%m%d`
 
 # Signer avec le certificat CA ROOT
 CURRENT_PATH=$PWD
