2e8ac613cf1e4126a0f9a7634253de0b5ca74f0a
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-04 11:14:50 -0500

    Fix fichier configuration nginx, envsubst.

--- docker/dockerfiles/nginx/scripts/run.sh
+++ docker/dockerfiles/nginx/scripts/run.sh
@@ -3,6 +3,7 @@
 SECRET=/run/secrets
 NGINX=/usr/sbin/nginx
 CONF=/etc/nginx/conf.d
+REPLACE_VARS='$${URL_DOMAIN}'
 
 if [[ ! -d $SECRET ]]; then
   echo "Folder secret n'existe pas, on installe les certificats de test"
@@ -14,10 +15,10 @@ fi
 if [ -z $NGINX_NOOVERRIDE_CONF ]; then
   if [ -z $NGINX_CONFIG_FILE ]; then
     echo "Utilisation fichier configuration bundle default.conf"
-    envsubst < $APP_BUNDLE_DIR/sites-available/default.conf > $CONF/default.conf
+    envsubst $REPLACE_VARS < $APP_BUNDLE_DIR/sites-available/default.conf > $CONF/default.conf
   else
     echo "Utilisation fichier configuration dans bundle: $NGINX_CONFIG_FILE"
-    envsubst < $APP_BUNDLE_DIR/sites-available/$NGINX_CONFIG_FILE > $CONF/default.conf
+    envsubst $REPLACE_VARS < $APP_BUNDLE_DIR/sites-available/$NGINX_CONFIG_FILE > $CONF/default.conf
   fi
 fi
 
