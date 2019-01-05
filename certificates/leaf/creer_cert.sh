cf28a73c103766810261e9c691875dff270b37a2
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-05 11:07:23 -0500

    Fix bug CNF pour nom org (MG_NOM)

--- certificates/leaf/creer_cert.sh
+++ certificates/leaf/creer_cert.sh
@@ -5,14 +5,14 @@ if [ -z $NOM_OU ]; then
   echo "Il faut fournir un nom de la MilleGrille pour le certificat"
   exit 1
 fi
-export NOM_OU  # Utilise par CNF
+export $NOM_OU  # Utilise par CNF
 
 DOMAIN_SUFFIX=$2
 if [ -z $DOMAIN_SUFFIX ]; then
   echo "Il faut fournir le suffixe du domaine de la MilleGrille"
   exit 1
 fi
-export DOMAIN_SUFFIX  # Utilise par CNF
+export $DOMAIN_SUFFIX  # Utilise par CNF
 
 # Importer fonctions
 source fonctions_creer_certs.sh
