a59a8a4ccfd5a0180630a6644c03ef4b03c243a5
Mathieu Dugre <mathieu@infraserv2.maple.mdugre.info>
2019-01-05 09:14:15 -0500

    Travail script creation cert

--- certificates/leaf/creer_cert.sh
+++ certificates/leaf/creer_cert.sh
@@ -8,19 +8,30 @@ if [ -z $NOM_OU ]; then
 fi
 
 CNF_FILE=openssl-millegrille.cnf
-PRIVATE_PATH=~/certificates/millegrilles/privkeys
+SECURE_PATH=~/certificates/millegrilles
+PRIVATE_PATH=$SECURE_PATH/privkeys
 KEY=$PRIVATE_PATH/${NOM_OU}.pem
 
-openssl req \
-        -config $CNF_FILE \
-        -newkey rsa:8192 \
-        -sha512 \
-        -nodes \
-        -out ${NOM_OU}.csr -outform PEM \
-        -keyout $KEY -keyform PEM
+mkdir -p $PRIVATE_PATH
+mkdir -p $CERT_PATH
 
-# Creer backup de la cle
-chmod 400 $KEY
-cp $KEY $KEY.`date +%Y%m%d`
+requete() {
+  CNF_FILE=$1
+  NOM_OU=$2
+  KEY=$3
 
-# Signer avec le certificat signing
+  openssl req \
+          -config $CNF_FILE \
+          -newkey rsa:8192 \
+          -sha512 \
+          -nodes \
+          -out ${NOM_OU}.csr -outform PEM \
+          -keyout $KEY -keyform PEM
+
+  # Creer backup de la cle
+  chmod 400 $KEY
+  cp $KEY $KEY.`date +%Y%m%d`
+}
+
+requete $CNF_FILE $NOM_OU $KEY
+
