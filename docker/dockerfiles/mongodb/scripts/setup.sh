7f1fe35f7db47a5194f35b7fdb0dcc604472af02
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-05 23:05:05 -0500

    Build 1 pour Mongo.

--- docker/dockerfiles/mongodb/scripts/setup.sh
+++ docker/dockerfiles/mongodb/scripts/setup.sh
@@ -5,11 +5,6 @@ echo "APP_SOURCE_DIR = $APP_SOURCE_DIR"
 echo "APP_BUNDLE_DIR = $APP_BUNDLE_DIR"
 
 mkdir -p $APP_BUNDLE_DIR
-mkdir -p $APP_SOURCE_DIR
-
-echo "Copier certs vers $APP_BUNDLE_DIR"
-mkdir -p $APP_BUNDLE_DIR/certs
-mv $APP_SOURCE_DIR/millegrilles.ca.cert.pem $APP_BUNDLE_DIR/certs
 
 echo "Fichiers dans $APP_BUNDLE_DIR :"
 find $APP_BUNDLE_DIR
