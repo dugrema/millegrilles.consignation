b7c9cdeb7af9fa0833f29114efcec9a931f67e7d
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-06 11:13:32 -0500

    Modification config.js et db.js dans mongo-express.
    Supporte maintenant les certificats.

--- docker/dockerfiles/mongo-express/scripts/setup.sh
+++ docker/dockerfiles/mongo-express/scripts/setup.sh
@@ -6,8 +6,11 @@ chmod 755 $SRC_FOLDER/scripts/run.sh
 mkdir -p $BUNDLE_FOLDER
 cp $SRC_FOLDER/scripts/run.sh $BUNDLE_FOLDER
 
+echo "Override des fichiers config.js et lib/db.js pour supporter certificats"
+cp $SRC_FOLDER/config.js /node_modules/mongo-express
+cp $SRC_FOLDER/db.js /node_modules/mongo-express/lib
+
 echo "Suppression du folder $SRC_FOLDER"
 rm -rf $SRC_FOLDER
 
 echo "Setup termine"
-
