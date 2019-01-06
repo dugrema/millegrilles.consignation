d3b33757f0831441c28c23f3e9f1c8b24da3df9c
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-06 13:06:52 -0500

    Test pour creer certs avec dns sans suffixe.
    Peut servir avec les servers dans une swarm (pas besoin de suffixe pour 
    les noms de services).

--- certificates/signing/sign_request.sh
+++ certificates/signing/sign_request.sh
@@ -5,7 +5,7 @@ source fonctions_cert.sh
 NOM_OU=$1
 
 if [ -z $NOM_OU ]; then
-  echo "Il faut fournir les parametres suivants: server nom_noeud"
+  echo "Il faut fournir les parametres suivants: nom_noeud"
 fi
 
 # Creer et signer un nouveau certificat
