28f232c8b7a1d05443b212038b789f7fe3e00bf9
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-14 19:35:41 -0500

    Travail sur deploiement python et meteor

---
+++ docker/dockerfiles/meteor-md/scripts/post-install-cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+printf "\n[-] Performing post install cleanup...\n\n"
+
+# Clean out docs
+rm -rf /usr/share/{doc,doc-base,man,locale,zoneinfo}
+
+# Clean out package management dirs
+rm -rf /var/lib/{cache,log}
+
+# clean additional files created outside the source tree
+rm -rf /root/{.cache,.config,.local}
+rm -rf /tmp/*
+
+# remove os dependencies
+#apt-get purge -y --auto-remove bzip2 python git wget
+apt-get purge -y --auto-remove bzip2 git wget
+apt-get -y autoremove
+rm -rf /var/lib/apt/lists/*
