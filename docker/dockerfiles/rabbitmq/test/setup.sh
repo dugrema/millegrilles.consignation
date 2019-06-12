cf5f3855f3ce40973ce5a895be1c8ae0135f00a0
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-06-12 14:40:42 -0400

    Ajout script test pour MQ, certs par defaut.

--- docker/dockerfiles/rabbitmq/test/setup.sh
+++ docker/dockerfiles/rabbitmq/test/setup.sh
@@ -4,14 +4,8 @@ cd /opt/rabbitmq/dist
 
 mkdir -p /run/secrets
 
-cat millegrilles.RootCA.pem \
-    millegrille_dev2.maple.mdugre.info.cert.pem.fullchain > \
-    /run/secrets/pki.millegrilles.ssl.CAchain
+cp pki.millegrilles.* /run/secrets/
+chown root:rabbitmq /run/secrets/*
+chmod 440 /run/secrets/*
 
-cp dev2.maple.mdugre.info.cert.pem /run/secrets/pki.millegrilles.ssl.cert
-cp dev2.maple.mdugre.info.key.pem /run/secrets/pki.millegrilles.ssl.key
-
-cp dev2.maple.mdugre.info.cert.pem /run/secrets/pki.millegrilles.web.cert
-cp dev2.maple.mdugre.info.key.pem /run/secrets/pki.millegrilles.web.key
-
-cp -f rabbitmq.config /etc/rabbitmq
+# cp -f rabbitmq.config /etc/rabbitmq
