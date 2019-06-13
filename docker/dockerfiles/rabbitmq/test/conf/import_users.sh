e03153c7a68394aca834eb5db73edd23bac005a8
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-06-13 16:20:25 -0400

    Travail script pour ajouter usagers.

--- docker/dockerfiles/rabbitmq/test/conf/import_users.sh
+++ docker/dockerfiles/rabbitmq/test/conf/import_users.sh
@@ -1,26 +1,38 @@
 #!/usr/bin/env bash
 
+function ajouter_usager {
+  FAU_USER=$1
+  rabbitmqctl add_user $FAU_USER CLEAR_ME < /dev/null
+  rabbitmqctl clear_password $FAU_USER < /dev/null
+}
+
 function ajouter_usager_noeud {
   F_USER=$1
   F_VHOST=$2
+
   echo "Ajouter noeud $F_USER sur vhost $F_VHOST"
+  ajouter_usager $F_USER
 
   # Input via /dev/null pour eviter d'arreter la boucle
-  rabbitmqctl add_user $F_USER CLEAR_ME < /dev/null
-  rabbitmqctl clear_password $F_USER < /dev/null
-  rabbitmqctl set_permissions -p $F_VHOST $F_USER "^amq\.gen\-.*$" "^amq\.gen\-.*$" "^(amq\.gen\-.*|millegrilles\.evenements)$" < /dev/null
+  rabbitmqctl set_permissions -p $F_VHOST $F_USER "^amq\.gen\-.*$" "^amq\.gen\-.*$" "^(amq\.gen\-.*|millegrilles\.noeuds)$" < /dev/null
   rabbitmqctl set_topic_permissions -p $F_VHOST $F_USER "millegrilles.noeuds" "nouvelle\.transaction" "topic\.test" < /dev/null
 }
 
 function ajouter_usager_inter {
-  echo "Ajouter inter $1 sur vhost $2"
-  # rabbitmqctl set_permissions -p sansnom lecteur_1 "^amq\.gen\-.*$" "^amq\.gen\-.*$" "^(amq\.gen\-.*|millegrilles\.evenements)$"
-  # rabbitmqctl set_topic_permissions -p sansnom lecteur_1 "millegrilles.evenements" "" "topic\.test"
+  F_USER=$1
+  F_VHOST=$2
+
+  echo "Ajouter noeud $F_USER sur vhost $F_VHOST"
+  ajouter_usager $F_USER
+
+  # Input via /dev/null pour eviter d'arreter la boucle
+  rabbitmqctl set_permissions -p $F_VHOST $F_USER "^amq\.gen\-.*$" "^amq\.gen\-.*$" "^(amq\.gen\-.*|millegrilles\.inter)$" < /dev/null
+  rabbitmqctl set_topic_permissions -p $F_VHOST $F_USER "millegrilles.inter" "nouvelle\.transaction" "topic\.test" < /dev/null
 }
 
 filename=$1
 while IFS= read -r line; do
-  IFS=' ' read -r -a parametres <<< "$line"
+  IFS=';' read -r -a parametres <<< "$line"
   USAGER=${parametres[0]}
   VHOST=${parametres[1]}
   ROLE=${parametres[2]}
