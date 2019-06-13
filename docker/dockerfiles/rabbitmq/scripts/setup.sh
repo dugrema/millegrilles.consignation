b0e122885b1343d04228b658b001290baa6ca170
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-06-13 13:17:32 -0400

    Ajout configuration de base pour vhost, middleware au demarrage de MQ.

--- docker/dockerfiles/rabbitmq/scripts/setup.sh
+++ docker/dockerfiles/rabbitmq/scripts/setup.sh
@@ -9,6 +9,9 @@ echo "Plugins ajoutes"
 echo "Copier la configuration des usagers"
 mkdir -p $APP_BUNDLE_FOLDER
 cp $APP_SOURCE_FOLDER/config/rabbitmq.config /etc/rabbitmq
+cp $APP_SOURCE_FOLDER/scripts/run.sh /usr/local/sbin
+cp $APP_SOURCE_FOLDER/scripts/update_definitions.sh $APP_BUNDLE_FOLDER
+cp $APP_SOURCE_FOLDER/config/definitions.json $APP_BUNDLE_FOLDER
 cp $APP_SOURCE_FOLDER/config/definitions_template.json $APP_BUNDLE_FOLDER
 
 # Installer setcap pour permettre de demarrer sur port 443
@@ -21,3 +24,4 @@ setcap 'cap_net_bind_service=+ep' /usr/lib/erlang/erts-9.3.3.3/bin/beam.smp
 # Cleanup, supprimer le repertoire src/
 cd /
 rm -rf $APP_SOURCE_FOLDER
+ln -s /usr/local/sbin/run.sh
--- docker/dockerfiles/rabbitmq/test/docker-run.sh
+++ docker/dockerfiles/rabbitmq/test/docker-run.sh
@@ -4,4 +4,4 @@ docker run -it \
 -p 15671:443 -p 5671-5673:5671-5673 \
 -e MG_NOM_MILLEGRILLE=sansnom \
 -e MQ_MIDDLEWARE_SUBJECT="CN=dev2.maple.mdugre.info,OU=Middleware,O=dev2,L=Russell,ST=Ontario,C=CA" \
-mg_rabbitmq.x86_64:3.7-management_5_test
+mg_rabbitmq:test run.sh
