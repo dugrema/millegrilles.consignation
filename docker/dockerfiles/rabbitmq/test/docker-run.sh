fce36573f6e1b53950d5f4c9a0edab533fa2ae25
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-06-13 14:39:28 -0400

    Fix run.sh command dans Dockerfile pour MQ.

--- docker/dockerfiles/rabbitmq/test/docker-run.sh
+++ docker/dockerfiles/rabbitmq/test/docker-run.sh
@@ -4,4 +4,4 @@ docker run -it \
 -p 15671:443 -p 5671-5673:5671-5673 \
 -e MG_NOM_MILLEGRILLE=sansnom \
 -e MQ_MIDDLEWARE_SUBJECT="CN=dev2.maple.mdugre.info,OU=Middleware,O=dev2,L=Russell,ST=Ontario,C=CA" \
-mg_rabbitmq:test run.sh
+mg_rabbitmq:test
