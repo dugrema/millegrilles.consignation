60d9a43fc3dd357571d299c01d4b3f3319faa5c7
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-17 01:47:46 +0000

    Update scripts apres run sur pivoine

--- scripts/letsencrypt_www.sh
+++ scripts/letsencrypt_www.sh
@@ -12,29 +12,13 @@ fi
 DOMAIN=$1
 OPT_MILLEGRILLES_FOLDER=$2
 
-# sudo certbot certonly --dry-run --must-staple --staple-ocsp \
-# --config-dir $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt \
-# --expand --webroot -w $OPT_MILLEGRILLES_FOLDER/nginx/webroot \
-# -d www.$DOMAIN -d coupdoeil.$DOMAIN
-
-sudo certbot renew -n --must-staple --staple-ocsp \
+sudo certbot certonly --must-staple --staple-ocsp \
 --config-dir $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt \
 --expand --webroot -w $OPT_MILLEGRILLES_FOLDER/nginx/webroot \
---post-hook "./letsencrypt_renewhook.sh $1 $2"
+-d www.$DOMAIN -d coupdoeil.$DOMAIN
 
-# RESULT_CB=$?
-# Pour renew, utiliser commande renew et flag -n pour non-interactif
+# sudo certbot renew -n --must-staple --staple-ocsp \
+# --config-dir $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt \
+# --expand --webroot -w $OPT_MILLEGRILLES_FOLDER/nginx/webroot \
+# --post-hook "./letsencrypt_renewhook.sh $1 $2"
 
-# if [ $RESULT_CB -eq 0 ]; then
-#   # Ajouter les certificats dans docker secrets
-#   # chain, fullchain et key
-#   DATE_COURANTE=`date +%Y%m%d`
-#   sudo docker secret create pki.millegrilles.web.fullchain.$DATE_COURANTE \
-#     $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt/live/www.$DOMAIN/fullchain.pem
-#   sudo docker secret create pki.millegrilles.web.chain.$DATE_COURANTE \
-#     $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt/live/www.$DOMAIN/chain.pem
-#   sudo docker secret create pki.millegrilles.web.cert.$DATE_COURANTE \
-#     $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt/live/www.$DOMAIN/cert.pem
-#   sudo docker secret create pki.millegrilles.web.key.$DATE_COURANTE \
-#     $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt/live/www.$DOMAIN/privkey.pem
-# fi
