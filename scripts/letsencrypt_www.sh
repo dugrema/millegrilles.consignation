157749203b94634ab09e56b40c0df7db35e44ff0
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-16 20:49:41 -0400

    Ajout partie script www letsencrypt pour docker secrets automatique.

--- scripts/letsencrypt_www.sh
+++ scripts/letsencrypt_www.sh
@@ -12,7 +12,21 @@ fi
 DOMAIN=$1
 OPT_MILLEGRILLES_FOLDER=$2
 
-sudo certbot certonly --must-staple --staple-ocsp \
---config-dir $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt \
---expand --webroot -w $OPT_MILLEGRILLES_FOLDER/nginx/webroot \
--d www.$DOMAIN -d coupdoeil.$DOMAIN
+# sudo certbot certonly --must-staple --staple-ocsp \
+# --config-dir $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt \
+# --expand --webroot -w $OPT_MILLEGRILLES_FOLDER/nginx/webroot \
+# -d www.$DOMAIN -d coupdoeil.$DOMAIN
+
+# Pour renew, utiliser commande renew et flag -n pour non-interactif
+
+# Ajouter les certificats dans docker secrets
+# chain, fullchain et key
+DATE_COURANTE=`date +%Y%m%d`
+sudo docker secret create pki.millegrilles.web.fullchain.$DATE_COURANTE \
+  $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt/live/www.$DOMAIN/fullchain.pem
+sudo docker secret create pki.millegrilles.web.chain.$DATE_COURANTE \
+  $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt/live/www.$DOMAIN/chain.pem
+sudo docker secret create pki.millegrilles.web.cert.$DATE_COURANTE \
+  $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt/live/www.$DOMAIN/cert.pem
+sudo docker secret create pki.millegrilles.web.key.$DATE_COURANTE \
+  $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt/live/www.$DOMAIN/privkey.pem
