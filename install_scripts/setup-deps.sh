1129c0e4f7dda6c213b76da8083c3ea0f9926834
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-08-14 19:21:39 +0000

    Fix dep docker-compose (ajout sudo)

--- install_scripts/setup-deps.sh
+++ install_scripts/setup-deps.sh
@@ -33,9 +33,9 @@ installer_dockercompose() {
   # Noter que le docker-compose installe via apt est trop vieux.
   docker-compose version > /dev/null 2> /dev/null
   if [ $? -ne 0 ]; then
-    apt install curl
-    curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod 755 /usr/local/bin/docker-compose
+    sudo apt install curl
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod 755 /usr/local/bin/docker-compose
     echo "[OK] docker-compose installe"
   else
     echo "[INFO] docker-compose est deja installe"
@@ -44,6 +44,6 @@ installer_dockercompose() {
 
 # Sequence d'Installation
 
-installer_certbot_ppa
+# installer_certbot_ppa
 installer_docker
 installer_dockercompose
