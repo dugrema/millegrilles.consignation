7ea22da5aecf75df391ac50e6a8d1c97ff3b143f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-08-15 17:20:19 -0400

    Modification script pour lets encrypt sur public.

--- install_scripts/bin/letsencrypt_www.sh
+++ install_scripts/bin/letsencrypt_www.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-MESSAGE_AIDE_1='Il manque les parametres: DOMAIN OPT_MILLEGRILLES_FOLDER'
-MESSAGE_AIDE_2='Exemple: letsencrypt_www.sh maple.millegrilles.mdugre.info /opt/millegrilles/maple'
+MESSAGE_AIDE_1='Il manque les parametres: NOM_MILLEGRILLE DOMAIN_SUFFIX'
+MESSAGE_AIDE_2='Exemple: letsencrypt_www.sh maple maple.millegrilles.mdugre.info'
+
+NOM_MILLEGRILLE=$1
+DOMAIN_SUFFIX=$2
+
+# Importer variables d'environnement MilleGrilles
+source /opt/millegrilles/etc/variables.txt
+
+LISTE_APPS=( www coupdoeil )
+for NOM_DOMAINE in ${LISTE_APPS[@]}; do
+  NOMS_DOMAINES="$NOMS_DOMAINES -d $NOM_DOMAINE.$DOMAIN_SUFFIX"
+done
 
 if [ -z $2 ]; then
   echo $MESSAGE_AIDE_1
@@ -9,16 +20,18 @@ if [ -z $2 ]; then
   exit 1
 fi
 
-DOMAIN=$1
-OPT_MILLEGRILLES_FOLDER=$2
+echo NOMS_DOMAINES:  $NOMS_DOMAINES
+echo MG_FOLDER_LETSENCRYPT: $MG_FOLDER_LETSENCRYPT
+echo MG_FOLDER_NGINX_WWW_PUBLIC: $MG_FOLDER_NGINX_WWW_PUBLIC
 
-# sudo certbot certonly \
-# --config-dir $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt \
-# --expand --webroot -w $OPT_MILLEGRILLES_FOLDER/nginx/webroot \
-# -d www.$DOMAIN -d coupdoeil.$DOMAIN
+if [ ! -d $MG_FOLDER_LETSENCRYPT/live ]; then
+ sudo certbot certonly \
+   --config-dir $MG_FOLDER_LETSENCRYPT \
+   --expand --webroot -w $MG_FOLDER_NGINX_WWW_PUBLIC \
+   $NOMS_DOMAINES
+fi
 
-sudo certbot renew -n \
---config-dir $OPT_MILLEGRILLES_FOLDER/pki/letsencrypt \
---expand --webroot -w $OPT_MILLEGRILLES_FOLDER/nginx/webroot \
---post-hook "./letsencrypt_renewhook.sh $DOMAIN $OPT_MILLEGRILLES_FOLDER"
-
+# sudo certbot renew -n \
+# --config-dir $MG_FOLDER_LETSENCRYPT \
+# --expand --webroot -w $REPERTOIRE_NGINX \
+# --post-hook "./letsencrypt_renewhook.sh $DOMAIN $OPT_MILLEGRILLES_FOLDER"
