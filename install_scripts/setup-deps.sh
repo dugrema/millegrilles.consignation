1959efe8e0eacdb6d8a288f985a9ce718eebafb8
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-09-09 16:32:15 -0400

    Ajout installation docker compose via pip.
    Permet d'avoir docker-compose sur un RPi.

--- install_scripts/setup-deps.sh
+++ install_scripts/setup-deps.sh
@@ -33,10 +33,22 @@ installer_dockercompose() {
   # Noter que le docker-compose installe via apt est trop vieux.
   docker-compose version > /dev/null 2> /dev/null
   if [ $? -ne 0 ]; then
+
+    # Si on a une installation debian standard, on install via script
     sudo apt install curl
     sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    if [ $? -ne 0 ]; then
+      # Si echec (e.g. pour RPi), on installe vieille version via pip
+      echo "[WARN] Echec d'installtion docker-compose recent, on prend version 1.23"
+      pip install docker-compose~=1.23.0
+    fi
+
     sudo chmod 755 /usr/local/bin/docker-compose
-    echo "[OK] docker-compose installe"
+    if [ $? -eq 0 ]; then
+      echo "[OK] docker-compose installe"
+    else
+      echo "[FAIL] Echec installation docker-compose"
+    fi
   else
     echo "[INFO] docker-compose est deja installe"
   fi
