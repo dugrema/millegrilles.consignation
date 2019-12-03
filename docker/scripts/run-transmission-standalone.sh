786dde81269e26c463db34f83b42793b1c3cb811
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-12-03 20:54:31 +0000

    Torrent network host, repertoire torrents.

--- docker/scripts/run-transmission-standalone.sh
+++ docker/scripts/run-transmission-standalone.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
-MG_FOLDER=/opt/millegrilles/dev3
-WATCH_FOLDER=$MG_FOLDER/mounts/consignation/torrents
-STAGING_FOLDER=$MG_FOLDER/mounts/consignation/torrent_staging
+MG_TORRENTS_FOLDER=/opt/millegrilles/dev3/mounts/consignation/torrents
+TORRENTS_FOLDER=$MG_TORRENTS_FOLDER
+WATCH_FOLDER=$MG_TORRENTS_FOLDER/watch
+STAGING_FOLDER=$MG_TORRENTS_FOLDER/downloads
 
 docker container run -d --rm \
+  --network host \
   --name=transmission \
   -e PGID=980 \
   -e TZ=America/Toronto \
   -e USER=millegrilles \
   -e PASS=bwahahah1202 \
-  -p 9091:9091 \
-  -p 51413:51413 \
-  -p 51413:51413/udp \
   -v $STAGING_FOLDER:/downloads \
   -v $WATCH_FOLDER:/watch \
-  --restart unless-stopped \
+  -v $TORRENTS_FOLDER:/torrents \
+  -l ipv6.mapper.network=mg_ipv6 \
   docker.maceroc.com/transmission:2.94-r2-ls35
 
+#  --restart unless-stopped \
 #   -v path to data:/config \
+# -p 9091:9091 \
+# -p 51413:51413 \
+# -p 51413:51413/udp \
