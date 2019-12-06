d9592ee61e9e3fe4d50c079f6eeff8651f0f2b8f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-12-06 21:36:42 +0000

    Test transmission host/vip.

--- docker/scripts/run-transmission-standalone.sh
+++ docker/scripts/run-transmission-standalone.sh
@@ -5,6 +5,21 @@ TORRENTS_FOLDER=$MG_TORRENTS_FOLDER
 WATCH_FOLDER=$MG_TORRENTS_FOLDER/watch
 STAGING_FOLDER=$MG_TORRENTS_FOLDER/downloads
 
+# docker container run -d --rm \
+#   --name=transmission \
+#   -p 9091:9091 \
+#   -p 51413:51413 \
+#   -p 51413:51413/udp \
+#   -e PGID=980 \
+#   -e TZ=America/Toronto \
+#   -e USER=millegrilles \
+#   -e PASS=bwahahah1202 \
+#   -v $STAGING_FOLDER:/downloads \
+#   -v $WATCH_FOLDER:/watch \
+#   -v $TORRENTS_FOLDER:/torrents \
+#   -l ipv6.mapper.network=mg_ipv6 \
+#   docker.maceroc.com/transmission:2.94-r2-ls35
+
 docker container run -d --rm \
   --network host \
   --name=transmission \
