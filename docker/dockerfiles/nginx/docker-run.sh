b935a26f60108ec68c0d97f1a34593a9a49715b2
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-12-07 21:53:38 +0000

    Ajout directive proxy X-Forwarded-For.

--- docker/dockerfiles/nginx/docker-run.sh
+++ docker/dockerfiles/nginx/docker-run.sh
@@ -2,7 +2,22 @@
 
 source image_info.txt
 
-docker run -d \
-  -p 80:80 \
-  -p 443:443 \
-  $REPO/$NAME.x86_64:$VERSION
+CONF_FOLDER=`pwd`/config-test
+
+# docker run -d --rm \
+#   -p 80:80 \
+#   -p 443:443 \
+#   $REPO/$NAME:$VERSION
+
+# docker run --rm \
+#   -p 80:80 \
+#   -p 443:443 \
+#   -v $CONF_FOLDER:/etc/nginx/conf.d \
+#   -v /home/mathieu/mgdev/certs:/certs \
+#   $REPO/$NAME:$VERSION
+
+docker run --rm \
+  --network host \
+  -v $CONF_FOLDER:/etc/nginx/conf.d \
+  -v /home/mathieu/mgdev/certs:/certs \
+  $REPO/$NAME:$VERSION
