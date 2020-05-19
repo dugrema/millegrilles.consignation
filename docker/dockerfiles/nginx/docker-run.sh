59b1772768dc4036adbe38299b35acf10019e5b2
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-05-19 23:06:32 +0000

    Test auth nginx/maitre des comptes

--- docker/dockerfiles/nginx/docker-run.sh
+++ docker/dockerfiles/nginx/docker-run.sh
@@ -4,12 +4,15 @@ source image_info.txt
 
 CONF_FOLDER=`pwd`/config-test
 
+# Override version (e.g. pour utiliser x86_64_...)
+VERSION=x86_64_1.18_0
+
 # docker run -d --rm \
 #   -p 80:80 \
 #   -p 443:443 \
 #   $REPO/$NAME:$VERSION
 
-# docker run --rm \
+# docker run --rm -it \
 #   -p 80:80 \
 #   -p 443:443 \
 #   -v $CONF_FOLDER:/etc/nginx/conf.d \
