716fc23a9c005ed49b9df9218beedfe616d1ab7d
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-07-08 00:45:51 +0000

    Retour a riftbit blynk
    L'image avec server officiel est enorme

--- docker/dockerfiles/blynk/build.sh
+++ docker/dockerfiles/blynk/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ -d blynk-server ]; then
-  git -C blynk-server pull
+if [ -d docker-blynk ]; then
+  git -C docker-blynk pull
 else
-  git clone https://github.com/blynkkk/blynk-server.git
-  ln -s blynk-server/server/Docker docker-blynk
+  git clone https://github.com/riftbit/docker-blynk.git
+  # ln -s blynk-server/server/Docker docker-blynk
 fi
 
 cp image_info.txt docker-blynk
