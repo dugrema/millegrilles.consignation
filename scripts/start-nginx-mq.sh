42529857cd2b4ef5f40a1b48ac269cce9dae3dd2
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-01-19 12:22:27 -0500

    Script nginxadmin avec mq, mongoexpress
    Utilisation service pour exposer

--- scripts/start-nginx-mq.sh
+++ scripts/start-nginx-mq.sh
@@ -2,9 +2,21 @@
 
 CURRENTPWD=`pwd`
 CONF="${CURRENTPWD}/nginx-mq.conf"
+CERTS="/home/mathieu/mgdev/certs"
 
-docker run --rm \
-  -v "${CONF}:/etc/nginx/conf.d/nginx-mq.conf:ro" \
-  -v /home/mathieu/mgdev/certs:/certs:ro \
+#docker run --rm \
+#  -v "${CONF}:/etc/nginx/conf.d/nginx-mq.conf:ro" \
+#  -v /home/mathieu/mgdev/certs:/certs:ro \
+#  -p 1443:1443 \
+#  -p 1444:1444 \
+#  --network millegrille_net \
+#  nginx
+
+docker service create \
+  --name nginxadmin \
+  --mount "type=bind,source=${CONF},destination=/etc/nginx/conf.d/nginx-mq.conf,ro=true" \
+  --mount "type=bind,source=${CERTS},destination=/certs,ro=true" \
   -p 1443:1443 \
+  -p 1444:1444 \
+  --network millegrille_net \
   nginx
