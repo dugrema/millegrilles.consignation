d6ee8658aac9fe140588ea9dec8129f98c6b9f57
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-06-07 17:01:54 -0400

    Update config pour nouveau instance

--- scripts/redis.sh
+++ scripts/redis.sh
@@ -1,9 +1,10 @@
-CONTAINER=814bc2dba323
+CONTAINER=58d8cd8165ab
 PATH_PASSWD=/var/opt/millegrilles/secrets/passwd.redis.20220507194334
 
 PASSWORD=`cat $PATH_PASSWD`
 
-echo Utiliser "auth client_nodejs $PASSWORD"
+echo Utiliser:
+echo "auth client_nodejs $PASSWORD"
 
 docker exec -it $CONTAINER redis-cli \
   --tls --cacert /run/secrets/millegrille.cert.pem --cert /run/secrets/cert.pem --key /run/secrets/key.pem
