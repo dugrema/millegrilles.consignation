738f01d6544d0a80972d1c7e442e9495cd025e94
Mathieu Dugre <mathieu.dugre@mdugre.info>
2025-06-05 13:25:38 -0400

    Upgrading to rabbitmq 4.1

--- docker/dockerfiles/rabbitmq/scripts/run.sh
+++ docker/dockerfiles/rabbitmq/scripts/run.sh
@@ -11,14 +11,6 @@ if [ ! -z $WEB_KEY ]; then ln -sf $WEB_KEY $REP_KEYS/webkey.pem; fi
 if [ ! -z $MG_CA ]; then
   ln -sf $MG_CA $REP_CERTS/calist.cert.pem
 fi
-#else
-#  echo "Batir la chaine de certificats CA a partir de cert.pem"
-  # Recuperer les certificats CA, enlever le premier de la liste qui est le
-  # cert local.
-#  awk -v fichier_cert=$REP_CERTS/calist.cert.pem 'BEGIN {c=0;} /BEGIN CERT/{c++} { if(c > 1) print > fichier_cert;}' < $REP_CERTS/cert.pem
-#fi
-
-# cat $REP_CERTS/cert.pem $REP_CERTS/calist.cert.pem > $REP_CERTS/cert_chainall.pem
 
 # Verification de quel fichier de configuration on utilise
 if [ ! -z $CONFIG_FILE ]; then
