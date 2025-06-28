971b4ee642bcd1fabf067d784c05790a9887b557
Mathieu Dugre <mathieu.dugre@mdugre.info>
2025-06-28 18:21:15 -0400

    New approach, no media table

--- scripts/python/fix_media_summary.py
+++ scripts/python/fix_media_summary.py
@@ -2,24 +2,11 @@ from pymongo.database import Database
 
 
 def fix_media_summary(db: Database):
-    collection_media = db['GrosFichiers/media']
-    collection_fichiersRep = db['GrosFichiers/fichiersRep']
+    collection_fichiersrep = db['GrosFichiers/fichiersRep']
 
     count = 0
 
     pipeline = [
-        # {"$match": {"images.small.nonce": {"$exists": True}}},
-        # {"$project": {"fuuid": 1}},
-        # {"$lookup": {
-        #     "from": "GrosFichiers/fichiersRep",
-        #     "localField": "fuuid",
-        #     "foreignField": "fuuids_versions.0",
-        #     "as": "fichiersrep",
-        #     "pipeline": [
-        #         {"$project": {"tuuid": 1, "mimetype": 1, "flag_summary": 1, "supprime": 1}}
-        #     ]
-        # }},
-        # {"$match": {"fichiersrep.supprime": False, "fichiersrep.flag_summary": True, "fichiersrep.mimetype": {"$regex": "^image/"}}},
         {"$match": {"supprime": False, "flag_summary": True, "type_node": "Fichier", "mimetype": {"$regex": "^image/"}}},
         {"$lookup": {
             "from": "GrosFichiers/fileComments",
@@ -30,14 +17,11 @@ def fix_media_summary(db: Database):
                 {"$project": {"comment_id": 1}}
             ]
         }},
-        {"$match": {"comments.0": {"$exists": False}}}
+        {"$match": {"comments.0": {"$exists": False}}},
+        {"$limit": 20}
     ]
 
-    # cursor_media = collection_media.aggregate(pipeline)
-    # for media in cursor_media:
-    #     # print(media)
-    #     fichiersrep = media['fichiersrep'][0]
-    cursor_fichiersrep = collection_fichiersRep.aggregate(pipeline)
+    cursor_fichiersrep = collection_fichiersrep.aggregate(pipeline)
     for fichiersrep in cursor_fichiersrep:
         # print(fichiersrep)
         tuuid = fichiersrep['tuuid']
