e302abf2d986cac48baa0893853408aac90aeb93
Mathieu Dugre <mathieu.dugre@mdugre.info>
2025-06-28 16:56:58 -0400

    New approach, no media table

--- scripts/python/fix_media_summary.py
+++ scripts/python/fix_media_summary.py
@@ -3,25 +3,27 @@ from pymongo.database import Database
 
 def fix_media_summary(db: Database):
     collection_media = db['GrosFichiers/media']
+    collection_fichiersRep = db['GrosFichiers/fichiersRep']
 
     count = 0
 
     pipeline = [
-        {"$match": {"images.small.nonce": {"$exists": True}}},
-        {"$project": {"fuuid": 1}},
-        {"$lookup": {
-            "from": "GrosFichiers/fichiersRep",
-            "localField": "fuuid",
-            "foreignField": "fuuids_versions.0",
-            "as": "fichiersrep",
-            "pipeline": [
-                {"$project": {"tuuid": 1, "mimetype": 1, "flag_summary": 1, "supprime": 1}}
-            ]
-        }},
-        {"$match": {"fichiersrep.supprime": False, "fichiersrep.flag_summary": True, "fichiersrep.mimetype": {"$regex": "^image/"}}},
+        # {"$match": {"images.small.nonce": {"$exists": True}}},
+        # {"$project": {"fuuid": 1}},
+        # {"$lookup": {
+        #     "from": "GrosFichiers/fichiersRep",
+        #     "localField": "fuuid",
+        #     "foreignField": "fuuids_versions.0",
+        #     "as": "fichiersrep",
+        #     "pipeline": [
+        #         {"$project": {"tuuid": 1, "mimetype": 1, "flag_summary": 1, "supprime": 1}}
+        #     ]
+        # }},
+        # {"$match": {"fichiersrep.supprime": False, "fichiersrep.flag_summary": True, "fichiersrep.mimetype": {"$regex": "^image/"}}},
+        {"$match": {"supprime": False, "flag_summary": True, "type_node": "Fichier", "mimetype": {"$regex": "^image/"}}},
         {"$lookup": {
             "from": "GrosFichiers/fileComments",
-            "localField": "fichiersrep.tuuid",
+            "localField": "tuuid",
             "foreignField": "tuuid",
             "as": "comments",
             "pipeline": [
@@ -31,13 +33,16 @@ def fix_media_summary(db: Database):
         {"$match": {"comments.0": {"$exists": False}}}
     ]
 
-    cursor_media = collection_media.aggregate(pipeline)
-    for media in cursor_media:
-        # print(media)
-        fichiersrep = media['fichiersrep'][0]
+    # cursor_media = collection_media.aggregate(pipeline)
+    # for media in cursor_media:
+    #     # print(media)
+    #     fichiersrep = media['fichiersrep'][0]
+    cursor_fichiersrep = collection_fichiersRep.aggregate(pipeline)
+    for fichiersrep in cursor_fichiersrep:
+        # print(fichiersrep)
         tuuid = fichiersrep['tuuid']
 
-        if len(media['comments']) == 0:
+        if len(fichiersrep['comments']) == 0:
             print(tuuid)
             count += 1
             # collection_fichiersrep.update_one({"tuuid": tuuid}, {"$set": {"flag_summary": False}})
