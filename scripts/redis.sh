d82e42cd01046cbb9903cd89649b338cb579938a
Mathieu Dugre <mathieu.dugre@mdugre.info>
2026-07-15 17:47:49 -0400

    Updated redis script to get password from container.

--- scripts/redis.sh
+++ scripts/redis.sh
@@ -1,9 +1,10 @@
 #CONTAINER=6d3c0258b548
-CONTAINER=`docker container ls --filter name="^redis\." -q`
+CONTAINER=`docker container ls --filter name="-redis-1" -q`
 #PATH_PASSWD=/var/opt/millegrilles/secrets/passwd.redis.20220518115418
-PATH_PASSWD=/var/opt/millegrilles/secrets/passwd.redis.txt
+PATH_PASSWD="/run/secrets/passwd.redis.txt"
 
-PASSWORD=`cat $PATH_PASSWD`
+# PASSWORD=`cat $PATH_PASSWD`
+PASSWORD=`docker exec $CONTAINER cat $PATH_PASSWD`
 
 echo Utiliser:
 echo "auth client_nodejs $PASSWORD"
